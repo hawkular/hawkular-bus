9b3f067554345230e78b112bc52a4f617e7bc15f
Stefan Negrea <snegrea@redhat.com>
2015-07-17 12:03:44 -0500

    [HAWKULAR-459] Fix the serialization issues by allowing the BasicMessage methods to control the content. This allows for symmetric deserialization & serialization of ObjectMessage and included payload.

--- hawkular-bus-common/src/test/java/org/hawkular/bus/common/ObjectMessageTest.java
+++ hawkular-bus-common/src/test/java/org/hawkular/bus/common/ObjectMessageTest.java
@@ -16,15 +16,17 @@
  */
 package org.hawkular.bus.common;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class ObjectMessageTest {
+
     @Test
     public void testObjectMessageNoArgConstructor() {
         ObjectMessage msg = new ObjectMessage();
         try {
             msg.getObject();
-            assert false : "should have failed - didn't tell it the class";
+            Assert.fail("should have failed - didn't tell it the class");
         } catch (IllegalStateException expected) {
         }
         /*
@@ -33,16 +35,16 @@ public class ObjectMessageTest {
         msg.setMessage("\"foo\"");
         msg.setObjectClass(String.class);
         Object o = msg.getObject();
-        assert o.getClass() == String.class;
-        assert o.toString().equals("foo");
+        Assert.assertEquals(String.class, o.getClass());
+        Assert.assertEquals("foo", o.toString());
     }
 
     @Test
     public void testObjectMessage() {
         ObjectMessage msg = new ObjectMessage(new String("bar"));
         Object o = msg.getObject();
-        assert o.getClass() == String.class;
-        assert o.toString().equals("bar");
+        Assert.assertEquals(String.class, o.getClass());
+        Assert.assertEquals("bar", o.toString());
     }
 
     @Test
@@ -54,27 +56,37 @@ public class ObjectMessageTest {
         // pass object to constructor
         ObjectMessage msg = new ObjectMessage(myobj);
         Object o = msg.getObject();
-        assert o.getClass() == MyObj.class;
-        assert ((MyObj) o).letters.equals("abc");
-        assert ((MyObj) o).number == 123;
-
-        assert msg.toJSON().equals("{\"letters\":\"abc\",\"number\":123}") : msg.toJSON();
-        assert msg.toJSON().equals(msg.getMessage()) : msg.getMessage();
+        Assert.assertEquals(MyObj.class, o.getClass());
+        Assert.assertEquals("abc", ((MyObj) o).letters);
+        Assert.assertEquals(123, ((MyObj) o).number);
+        Assert.assertNotEquals(msg.toJSON(), msg.getMessage());
 
         // pass class to constructor
         msg = new ObjectMessage(MyObj.class);
-        msg.setMessage("{\"letters\":\"xyz\",\"number\":987}");
-        assert msg.toJSON().equals("{\"letters\":\"xyz\",\"number\":987}") : msg.toJSON();
-        assert ((MyObj) msg.getObject()).letters.equals("xyz");
-        assert ((MyObj) msg.getObject()).number == 987;
+        msg.setMessage("{\"letters\":\"xyz\",\"number\":987}}");
+        Assert.assertEquals("xyz", ((MyObj) msg.getObject()).letters);
+        Assert.assertEquals(987, ((MyObj) msg.getObject()).number);
 
         // pass nothing to constructor
         msg = new ObjectMessage();
         msg.setObjectClass(MyObj.class);
-        msg.setMessage("{\"letters\":\"xyz\",\"number\":987}");
-        assert msg.toJSON().equals("{\"letters\":\"xyz\",\"number\":987}") : msg.toJSON();
-        assert ((MyObj) msg.getObject()).letters.equals("xyz");
-        assert ((MyObj) msg.getObject()).number == 987;
+        msg.setMessage("{\"letters\":\"xzy\",\"number\":1987}}");
+        Assert.assertEquals("xzy", ((MyObj) msg.getObject()).letters);
+        Assert.assertEquals(1987, ((MyObj) msg.getObject()).number);
+    }
+
+    @Test
+    public void testSerializingObjectMessageItself() {
+        MyObj myobj = new MyObj();
+        myobj.letters = "abc";
+        myobj.number = 123;
+        ObjectMessage msg = new ObjectMessage(myobj);
+        String jsonPayload = msg.toJSON();
+
+        ObjectMessage objectUnderTest = BasicMessage.fromJSON(jsonPayload, ObjectMessage.class);
+        objectUnderTest.setObjectClass(MyObj.class);
+        Assert.assertEquals("abc", ((MyObj) objectUnderTest.getObject()).letters);
+        Assert.assertEquals(123, ((MyObj) objectUnderTest.getObject()).number);
     }
 }
 
